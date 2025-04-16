dcaff0201bd6a6f2197fed41702de8f669c0f549
mhahm2 <mhahm2@umbc.edu>
2025-04-15 23:46:06 -0400

    create our matrices (taken from example given in homework)

---
+++ Homework_8/matrix_cpp/main.cpp
@@ -0,0 +1,18 @@
+#include "matrix.h"
+using namespace std;
+
+int main() {
+    Matrix A({
+        {6, 4},
+        {8, 3}
+    });
+    Matrix B({
+        {1, 2, 3},
+        {4, 5 ,6}
+    });
+    Matrix C({
+        {2, 4 ,6},
+        {1, 3, 5}
+    });
+    
+}
