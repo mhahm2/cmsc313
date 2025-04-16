80a54ae1bcf440e52215bdc71fc5872ed2e3ca59
mhahm2 <mhahm2@umbc.edu>
2025-04-16 18:38:04 -0400

    added and implemented multiply_matrices function

--- Homework_8/matrix_c/matrix.c
+++ Homework_8/matrix_c/matrix.c
@@ -21,3 +21,16 @@ void transpose(int rows, int cols, int matrix[rows][cols],
     }
     
 }
+
+void multiply_matrices(int r1, int c1, int mat1[r1][c1], 
+    int r2, int c2, int mat2[r2][c2], int result[c1][c2]) {
+    // for-loop to handle the matrix multiplication logic
+    for (int i = 0; i < r1; ++i) {
+        for (int j = 0; j < c2; ++j) {
+            result[i][j] = 0;
+            for(int k = 0; k < c1; ++k) {
+                result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+}
