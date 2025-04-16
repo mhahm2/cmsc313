1c8ba230f55d11f223b35440d74c7938377200df
mhahm2 <mhahm2@umbc.edu>
2025-04-16 19:34:00 -0400

    fixed some typos with my code

--- Homework_8/matrix_c/matrix.c
+++ Homework_8/matrix_c/matrix.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include "matrix.h"
 
-void scalar_multiply(int rows, int cols, int matrix[rows][cols], 
-    int scalar) {
+void scalar_multiply(int rows, int cols, int matrix[rows][cols], int scalar) {
     // for-loop to handle the scalar multiplication logic 
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
@@ -11,19 +10,17 @@ void scalar_multiply(int rows, int cols, int matrix[rows][cols],
     }
 }
 
-void transpose(int rows, int cols, int matrix[rows][cols], 
-    int result[cols][rows]) {
+void transpose(int rows, int cols, int matrix[rows][cols], int result[cols][rows]) {
     // for-loop to handle the transposition logic
     for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; +j) {
+        for (int j = 0; j < cols; ++j) {
             result[j][i] = matrix[i][j];
         }
     }
     
 }
 
-void multiply_matrices(int r1, int c1, int mat1[r1][c1], 
-    int r2, int c2, int mat2[r2][c2], int result[c1][c2]) {
+void multiply_matrices(int r1, int c1, int mat1[r1][c1], int r2, int c2, int mat2[r2][c2], int result[r1][c2]) {
     // for-loop to handle the matrix multiplication logic
     for (int i = 0; i < r1; ++i) {
         for (int j = 0; j < c2; ++j) {
@@ -35,8 +32,7 @@ void multiply_matrices(int r1, int c1, int mat1[r1][c1],
     }
 }
 
-void add_matrices(int rows, int cols, int mat1[rows][cols], 
-    int mat2[rows][cols], int result[rows][cols]) {
+void add_matrices(int rows, int cols, int mat1[rows][cols], int mat2[rows][cols], int result[rows][cols]) {
     // for-loop to hand matrix addition logic
     for (int i = 0; i < rows; ++i) {
         for (int j = 0; j < cols; ++j) {
--- Homework_8/matrix_c/matrix.h
+++ Homework_8/matrix_c/matrix.h
@@ -2,16 +2,10 @@
 #define MATRIX_H
 
 // member functions
-void scalar_multiply(int rows, int cols, int matrix[rows][cols], 
-    int scalar);
-void transpose(int rows, int cols, int matrix[rows][cols], 
-    int result[cols][rows]);
-void multiply_matrices(int r1, int c1, int mat1[r1][c1], 
-    int r2, int c2, int mat2[r2][c2],
-    int result[r1][r2]);
-void add_matrices(int rows, int cols, int mat1[rows][cols],
-    int mat2[rows][cols], 
-    int result[rows][cols]);
+void scalar_multiply(int rows, int cols, int matrix[rows][cols], int scalar);
+void transpose(int rows, int cols, int matrix[rows][cols], int result[cols][rows]);
+void multiply_matrices(int r1, int c1, int mat1[r1][c1], int r2, int c2, int mat2[r2][c2], int result[r1][r2]);
+void add_matrices(int rows, int cols, int mat1[rows][cols], int mat2[rows][cols], int result[rows][cols]);
 void print_matrix(int rows, int cols, int matrix[rows][cols]);
 
 #endif
