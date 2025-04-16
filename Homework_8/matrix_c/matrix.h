c75ebc38742c15ae2b409d862f484402606149db
mhahm2 <mhahm2@umbc.edu>
2025-04-16 18:27:30 -0400

    added function headers

--- Homework_8/matrix_c/matrix.h
+++ Homework_8/matrix_c/matrix.h
@@ -1,4 +1,17 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+// member functions
+void scalar_multiply(int rows, int cols, int matrix[rows][cols], 
+    int scalar);
+void transpose(int rows, int cols, int matrix[rows][cols], 
+    int result[cols][rows]);
+void multiply_matrices(int r1, int c1, int mat1[r1][r2], 
+    int r2, int c2, int mat2[r2][c2],
+    int result[r1][r2]);
+void add_matrices(int rows, int cols, int mat1[rows][cols],
+    int mat2[rows][cols], 
+    int result[rows][cols]);
+void print(int rows, int cols, int matrix[rows][cols]);
+
 #endif
