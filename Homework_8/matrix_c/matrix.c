dfb0c966235bc408337840fc98c8f46735ddb6fa
mhahm2 <mhahm2@umbc.edu>
2025-04-16 18:57:07 -0400

    added and implemented add_matrices and print functions

--- Homework_8/matrix_c/matrix.c
+++ Homework_8/matrix_c/matrix.c
@@ -34,3 +34,22 @@ void multiply_matrices(int r1, int c1, int mat1[r1][c1],
         }
     }
 }
+
+void add_matrices(int rows, int cols, int mat1[rows][cols], 
+    int mat2[rows][cols], int result[rows][cols]) {
+    // for-loop to hand matrix addition logic
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            result[i][j] = mat1[i][j] + mat2[i][j];
+        }
+    }
+}
+
+void print(int rows, int cols, int matrix[rows][cols]) {
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n")
+    }
+}
