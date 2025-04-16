5737a38708e80f62e185007b859bc5e512eab15c
mhahm2 <mhahm2@umbc.edu>
2025-04-16 18:34:32 -0400

    added scalar_multiply and tranpose function implementations (and fixed matrix.h)

--- Homework_8/matrix_c/matrix.c
+++ Homework_8/matrix_c/matrix.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
 #include "matrix.h"
+
+void scalar_multiply(int rows, int cols, int matrix[rows][cols], 
+    int scalar) {
+    // for-loop to handle the scalar multiplication logic 
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            matrix[i][j] *= scalar;
+        }
+    }
+}
+
+void transpose(int rows, int cols, int matrix[rows][cols], 
+    int result[cols][rows]) {
+    // for-loop to handle the transposition logic
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; +j) {
+            result[j][i] = matrix[i][j];
+        }
+    }
+    
+}
--- Homework_8/matrix_c/matrix.h
+++ Homework_8/matrix_c/matrix.h
@@ -6,7 +6,7 @@ void scalar_multiply(int rows, int cols, int matrix[rows][cols],
     int scalar);
 void transpose(int rows, int cols, int matrix[rows][cols], 
     int result[cols][rows]);
-void multiply_matrices(int r1, int c1, int mat1[r1][r2], 
+void multiply_matrices(int r1, int c1, int mat1[r1][c1], 
     int r2, int c2, int mat2[r2][c2],
     int result[r1][r2]);
 void add_matrices(int rows, int cols, int mat1[rows][cols],
