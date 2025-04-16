96c54adbad414652ec7dcb67acd767512e1bf4e4
mhahm2 <mhahm2@umbc.edu>
2025-04-15 23:12:53 -0400

    implemented constructors, scalarMultiply, and transpose functions

---
+++ Homework_8/matrix_cpp/matrix.cpp
@@ -0,0 +1,40 @@
+#include "matrix.h"
+using namespace std;
+
+// constructor
+Matrix::Matrix(int rows, int cols) {
+    this->rows = rows;
+    this->cols = cols;
+    data.resize(rows, vector<int>(cols, 0));
+}
+
+// copy constructor
+Matrix::Matrix(const vector<vector<int>> &values) {
+    // set row, col, and data of elements of matrix
+    rows = values.size();
+    cols = values[0].size();
+    data = values;
+}
+
+// scalarMultiply
+Matrix Matrix::scalarMultiply(int scalar) const {
+    Matrix result(rows, cols);
+    for(int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; j++) {
+            // multiply the matrix by the scalar
+            result.data[i][j] = data[i][j] * scalar;
+        }
+    }
+    return result;
+}
+
+// transpose
+Matrix Matrix::transpose() const {
+    Matrix result(cols, rows);
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            // swap value positions
+            result.data[j][i] = data[i][j]
+        }
+    }
+}
