a38c69665d7e44c9e7b14648913c0ca256dd6acb
mhahm2 <mhahm2@umbc.edu>
2025-04-15 23:49:23 -0400

    test matrix.cpp functionality (using function calls and printing the result)

--- Homework_8/matrix_cpp/main.cpp
+++ Homework_8/matrix_cpp/main.cpp
@@ -15,4 +15,19 @@ int main() {
         {1, 3, 5}
     });
     
+    // test matrix scaling
+    Matrix B_scaled = B.scalarMultiply(3);
+
+    // test matrix transposition
+    Matrix C_transposed = C.transpose();
+
+    // test matrix multiplication & addition
+    Matrix product = B_scaled.multiply(C_transposed);
+    Matrix D = A.add(product);
+
+    // print results (using print function)
+    cout << "Final result of D:" << endl;
+    D.print();
+
+    return 0;
 }
