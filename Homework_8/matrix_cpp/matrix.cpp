15af62d2ad21336aa684a11257b1d2ccc420e541
mhahm2 <mhahm2@umbc.edu>
2025-04-15 23:40:03 -0400

    fixed this nested for-loop

--- Homework_8/matrix_cpp/matrix.cpp
+++ Homework_8/matrix_cpp/matrix.cpp
@@ -50,7 +50,7 @@ Matrix Matrix::multiply(const Matrix &other) const {
     // for-loop (handle the matrix multiplication logic)
     for (int i = 0; i < rows; ++i) {
         // this specific for-loop handles VALUES within col
-        for (int j = 0; j < other.cols; ++j) {
+        for (int j = 0; j < other.cols; j++) {
             for (int k = 0; k < cols; ++k) {
                 result.data[i][j] += data[i][k] & other.data[k][j];
             }
