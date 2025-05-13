420ca3aaf8012b3d12f5260e0cbbc45bc2a671a0
mhahm2 <mhahm2@umbc.edu>
2025-05-12 22:44:37 -0400

    added inputBuf with data provided from assignment instructions

--- Homework_11/hw11_ascii.asm
+++ Homework_11/hw11_ascii.asm
@@ -7,3 +7,5 @@
 ;              and print the result.
 ;              It also uses a subroutine to convert each nibble (extra credit).
 
+SECTION .data
+inputBuf: db 0x83, 0x6A, 0x88, 0xDE, 0x9A, 0xC3, 0x54, 0x9A
