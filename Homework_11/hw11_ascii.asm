b7b82dd206cd7e4a2152cf6820a13d9ea3eb488d
mhahm2 <mhahm2@umbc.edu>
2025-05-12 22:41:01 -0400

    Added file header and description

---
+++ Homework_11/hw11_ascii.asm
@@ -0,0 +1,9 @@
+; Name: Michael Hahm
+; Email: mhahm2@umbc.edu
+; CMSC 313 - HW 11 (Data to Ascii)
+
+; Description: This program is meant to read bytes from an input buffer,
+;              convert them to ASCII hex, store them into an output buffer,
+;              and print the result.
+;              It also uses a subroutine to convert each nibble (extra credit).
+
